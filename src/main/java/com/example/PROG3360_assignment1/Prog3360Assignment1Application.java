86d21667db9bc685ca0b533f1bdefaeaaa1c4158
Ko-dany <iamgo910@gmail.com>
2026-01-29 19:04:00 -0500

    Set up Springboot project

---
+++ src/main/java/com/example/PROG3360_assignment1/Prog3360Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.PROG3360_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3360Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3360Assignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PROG3360_assignment1/Prog3360Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROG3360_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3360Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
